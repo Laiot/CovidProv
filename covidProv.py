369351bb47fe58acced7534415d86dd4213474a1
laiot <carmelo.sarta.main@gmail.com>
2020-03-31 22:16:07 +0200

    Fixed the X axis to show readable dates. Need more fixes.

--- covidProv.py
+++ covidProv.py
@@ -8,12 +8,15 @@ import re
 prov = pd.read_csv("resources/COVID-19/dati-province/dpc-covid19-ita-province.csv")
 prov['data'] = pd.to_datetime(prov['data']).dt.round('d')
 prov['totale_casi'] = prov['totale_casi'].astype('int')
-
+dates = ['02/26', '02/27', '02/28', '02/29', '02/26', '02/26', '02/26', '02/26', '02/26']
 prov.loc[prov['denominazione_provincia']=='Napoli', 'sigla_provincia']="NA"
 # prov['data'] = pd.to_datetime(prov['data'])
 # prov['data'] = prov['data'].dt.strftime('%d-%m')
 sns.lineplot(x='data', y='totale_casi', data=prov[prov['sigla_provincia']=='NA'])
+l = plt.xticks()
+plt.xticks(l[0], dates)
 plt.show()
 fig = sns.lineplot(x='data', y='totale_casi', data=prov[prov['sigla_provincia']=='NA'])
-
+l = plt.xticks()
+plt.xticks(l[0], dates)
 fig.figure.savefig('output.png')
