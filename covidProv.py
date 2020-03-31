aa9f7a51db166a5fddb38124426ad55a3ce54cea
laiot <carmelo.sarta.main@gmail.com>
2020-03-31 22:27:23 +0200

    Added all the dates. Need to fix the Y axis in order to make X axis readable.

--- covidProv.py
+++ covidProv.py
@@ -1,22 +1,18 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 import seaborn as sns
-import re
 
 # data
 prov = pd.read_csv("resources/COVID-19/dati-province/dpc-covid19-ita-province.csv")
 prov['data'] = pd.to_datetime(prov['data']).dt.round('d')
 prov['totale_casi'] = prov['totale_casi'].astype('int')
-dates = ['02/26', '02/27', '02/28', '02/29', '02/26', '02/26', '02/26', '02/26', '02/26']
-prov.loc[prov['denominazione_provincia']=='Napoli', 'sigla_provincia']="NA"
-# prov['data'] = pd.to_datetime(prov['data'])
-# prov['data'] = prov['data'].dt.strftime('%d-%m')
-sns.lineplot(x='data', y='totale_casi', data=prov[prov['sigla_provincia']=='NA'])
-l = plt.xticks()
-plt.xticks(l[0], dates)
+dates = ['02/26', '02/27', '02/28', '02/29', '03/01', '03/02', '03/03', '03/04', '03/05', '03/06', '03/07', '03/08', '03/09', '03/10', '03/11', '03/12', '03/13', '03/14', '03/15', '03/16', '03/17', '03/18', '03/19', '03/20', '03/21', '03/22', '03/23', '03/24', '03/25', '03,26', '03/27', '03/28', '03/29', '03/30', '03/31']
+prov.loc[prov['denominazione_provincia'] == 'Siracusa', 'sigla_provincia'] = "SR"
+sns.lineplot(x='data', y='totale_casi', data=prov[prov['sigla_provincia'] == 'SR'])
+latus = plt.xticks()
+plt.xticks(latus[0], dates)
 plt.show()
-fig = sns.lineplot(x='data', y='totale_casi', data=prov[prov['sigla_provincia']=='NA'])
-l = plt.xticks()
-plt.xticks(l[0], dates)
+fig = sns.lineplot(x='data', y='totale_casi', data=prov[prov['sigla_provincia'] == 'SR'])
+latus = plt.xticks()
+plt.xticks(latus[0], dates)
 fig.figure.savefig('output.png')
